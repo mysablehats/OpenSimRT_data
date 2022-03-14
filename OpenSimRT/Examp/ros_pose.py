acbcc3caf1d43ab1200785ad34739432dd36b7c0
Frederico B. K <frekle@kth.se>
2022-03-14 21:42:52 +0100

    static pose publisher

---
+++ OpenSimRT/Examp/ros_pose.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import rospy
+from geometry_msgs.msg import Pose
+
+def publisher():
+    pub = rospy.Publisher('pose', Pose, queue_size=1)
+    rospy.init_node('pose_publisher', anonymous=True)
+    rate = rospy.Rate(2) # Hz
+    while not rospy.is_shutdown():
+        p = Pose()
+        p.position.x = 0.5
+        p.position.y = -0.1
+        p.position.z = 1.0
+        # Make sure the quaternion is valid and normalized
+        p.orientation.x = 0.0
+        p.orientation.y = 0.0
+        p.orientation.z = 0.0
+        p.orientation.w = 1.0
+        pub.publish(p)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        publisher()
+    except rospy:
+        pass
